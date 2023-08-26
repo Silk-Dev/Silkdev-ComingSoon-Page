cf7974366b961ad2f745af939f433b7313526c9f
gharradhaifa <gharradhaifa6@gmail.com>
2023-08-26 09:44:01 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import HeroSection from './components/heroSection/HeroSection'
 import Header from './components/header/Header'
+import Slider from './components/slider/Slider'
 
 export default function Home() {
   return (
     <main>
+      <Slider/>
       <Header />
       <HeroSection />
       
