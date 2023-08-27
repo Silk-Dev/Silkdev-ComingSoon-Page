bcd07fc02a958060898fef2740e14f635d284ba9
bedouijesser <bedouijesser@gmail.com>
2023-08-27 22:34:00 +0100

    format + eager loading for hero section image

--- src/app/components/subMsg/SubMsg.tsx
+++ src/app/components/subMsg/SubMsg.tsx
@@ -1,26 +1,26 @@
 "use client";
-import React,{ useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from './subMsg.module.scss'
 import seccess from '/public/img/success.png'
 import error from '/public/img/error.png'
 const SubMsg = () => {
-    const [isError,setError] = useState(false)
+  const [isError, setError] = useState(false)
   return (
     <div className={styles.container} >
-        {!isError &&
+      {!isError &&
         <div className={styles.content}>
-        <Image width={220} height={100} alt='' src={seccess}/>
-        <p className={styles.textMsg}>Thank you for reaching out to us ! 
-           Your message has been successfully received.
-        </p>
+          <Image width={220} height={100} alt='' src={seccess} />
+          <p className={styles.textMsg}>Thank you for reaching out to us !
+            Your message has been successfully received.
+          </p>
         </div>}
-        {isError &&
+      {isError &&
         <div className={styles.content}>
-        <Image width={220} height={100} alt='' src={error}/>
-        <p className={styles.textMsg}>Oups !, it seems there was an issue with submitting your message
-Our team has been notified and is actively working to resolve this issue.
-        </p>
+          <Image width={220} height={100} alt='' src={error} />
+          <p className={styles.textMsg}>Oups !, it seems there was an issue with submitting your message
+            Our team has been notified and is actively working to resolve this issue.
+          </p>
         </div>}
     </div>
   )
