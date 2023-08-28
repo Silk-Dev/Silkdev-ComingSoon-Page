b9b9f837002321dddb40637065e7471267bd8299
Amal <bentalebamal8@gmail.com>
2023-08-28 21:50:30 +0100

    api config

--- src/app/components/form/Formm.tsx
+++ src/app/components/form/Formm.tsx
@@ -2,43 +2,25 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, Space , message, Steps, theme} from 'antd';
 import { SendOutlined } from '@ant-design/icons'
-const { TextArea } = Input;
-
 import styles from "./form.module.scss"
-import { log } from 'console';
+import { SendEmail } from '@/actions/SendEmail';
+const { TextArea } = Input;
 const Formm: React.FC = () => {
   const [form] = Form.useForm();
   const [submittable, setSubmittable] = useState(false);
   const [option, setOptions] = useState(0);
   const [step, setStep] = useState('one');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    project: '',
+    msg: '',
+  });
   const [selectedButton, setSelectedButton] = useState(null);
   
-  const steps = [
-    {
-      title: 'First',
-      content: 'first-content',
-    },
-    {
-      title: 'Second',
-      content: 'Second-content',
-    },
-    {
-      title: 'Last',
-      content: 'Last-content',
-    },
-  ];
   const { token } = theme.useToken();
   const [current, setCurrent] = useState(0);
 
-  const next = () => {
-    setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-
-  const items = steps.map((item) => ({ key: item.title, title: item.title }));
 
   const contentStyle: React.CSSProperties = {
     lineHeight: '260px',
@@ -66,12 +48,41 @@ const Formm: React.FC = () => {
     
   }, [values]);
 
+  const handleChange = (event:any) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+      
+    }));
+  };
   
+  const handleSubmit= async () => {
+    console.log('data',formData);
+    const { data, error } = await SendEmail(formData);
 
-  const handleButtonClick = (buttonId:any) => {
-  
+          if (error) {
+            console.log(error);
+            
+            return;
+          }
+          console.log("Email sent successfully!");
+    if (option > 0){
+      setStep('three');
+      setCurrent(current + 1);
+      
+     }else {console.log('set option please',option)}
+  };
+
+  const handleButtonClick = (buttonId:any,selectedOption:any) => {
+    setOptions(buttonId)
+    setFormData((prevData) => ({
+      ...prevData,
+      project: selectedOption,
+      
+    }));
     if (buttonId === selectedButton) {
-  
+      
       setSelectedButton(null);
     } else {
      
@@ -85,55 +96,78 @@ const Formm: React.FC = () => {
       <h1 className={styles.h1}>Let's Talk About Your Needs</h1>
       <div className={styles.custominput}>
       <Form.Item className={styles.custominputs} name="name" label={<span className={styles.customlabel}>Name</span>} rules={[{ required: true }]}>
-        <Input className={styles.input} placeholder='John from apple'/>
+        <Input className={styles.input} placeholder='John from apple'
+        value={formData.name}
+        name="name"
+        onChange={handleChange}/>
       </Form.Item>
       <Form.Item  className={styles.custominputs}  name="email" label={<span className={styles.customlabel}>Email</span>} rules={[{ required: true }]}>
-        <Input className={styles.input} type="email" placeholder='John@apple.com'/>
+        <Input className={styles.input} type="email" placeholder='John@apple.com'
+        value={formData.email}
+        name="email"
+        onChange={handleChange}/>
       </Form.Item>
       </div>
 <h1 className={styles.H1}>I'm intersted in ...</h1>
  <Space wrap className={styles.space}>
   <button
+  value={formData.project}
+  name="project"
   className={styles.buttons}
+  onChange={handleChange}
   style={{
     backgroundColor: selectedButton === 1 ? "#fff6ea" : "transparent",
-   
-    
     color: selectedButton === 1 ? "black" : "#fff6ea", 
   }}
 
-    onClick={() => {handleButtonClick(1);setOptions(1)}}
+    onClick={() => handleButtonClick(1,"showcase")}
   >
     Showcase Website
   </button>
   <button
   className={styles.buttons}
+   value="commerce"
+   name="project"
+   onChange={handleChange}
    style={{
     backgroundColor: selectedButton === 2 ? "#fff6ea" : "transparent",
     color: selectedButton === 2 ? "black" : "#fff6ea",
   }}
-    onClick={() => {handleButtonClick(2);setOptions(2)}}
+    onClick={() => handleButtonClick(2,"commerce")}
   >
     E-commerce website
   </button>
   <button
-  className={styles.buttons}
+    className={styles.buttons}
+    value={formData.project}
+    name="project"
+    onChange={handleChange}
     style={{
       backgroundColor: selectedButton === 3 ? "white" : "transparent",
       color: selectedButton === 3 ? "black" : "white",
     }}
-    onClick={() => {handleButtonClick(3);setOptions(3)}}
+    onClick={() => handleButtonClick(3,"branding")}
   >
     Brand consultation service
   </button>
  </Space>
  <h1 className={styles.H2}>Tell us more about your project</h1>
- <TextArea className={styles.textarea}rows={6} placeholder="Something about your great idea" maxLength={255} />
+ <TextArea 
+    className={styles.textarea}
+    rows={6} placeholder="Something about your great idea" 
+    maxLength={255} 
+    name="msg"
+    value={formData.msg}
+    onChange={handleChange}
+  />
 
  
  <button
  className={styles.submit}
         type="submit"
+        value={formData.project}
+        onChange={handleChange}
+        onClick={handleSubmit}
         disabled={!submittable}
         style={{
           cursor: submittable ? 'pointer' : 'not-allowed',fontSize:"18px",fontWeight:"500"
@@ -146,17 +180,23 @@ const Formm: React.FC = () => {
     </Form>
     
   <div className={styles.mobileslider}>
-  <Steps className={styles.stepsvertical} current={current} items={items} />
+  <Steps className={styles.stepsvertical} current={current}  />
       <div  style={contentStyle}>
       {step =='one' &&
    <Form className={styles.mobileform} form={form} name="validateOnly" layout="vertical" autoComplete="off">
    <h1 className={styles.h1}>Let's Talk About Your Needs</h1>
  <div className={styles.custominput}>
-   <Form.Item  className={styles.custominputs} name="name" label={<span className={styles.customlabel}>Name</span>} rules={[{ required: true }]}>
-     <Input className={styles.input} placeholder='John from apple'/>
-   </Form.Item>
+   <Form.Item className={styles.custominputs} name="name" label={<span className={styles.customlabel}>Name</span>} rules={[{ required: true }]}>
+        <Input className={styles.input} placeholder='John from apple'
+        value={formData.name}
+        name="name"
+        onChange={handleChange}/>
+      </Form.Item>
    <Form.Item  className={styles.custominputs}  name="email" label={<span className={styles.customlabel}>Email</span>} rules={[{ required: true }]}>
-        <Input className={styles.input} type="email" placeholder='John@apple.com'/>
+        <Input className={styles.input} type="email" placeholder='John@apple.com'
+        value={formData.email}
+        name="email"
+        onChange={handleChange}/>
       </Form.Item>
    <button
      className={styles.submit}
@@ -178,35 +218,42 @@ const Formm: React.FC = () => {
    <h1 className={styles.H1}>I'm intersted in ...</h1>
  <Space wrap className={styles.space}>
   <button
+  value={formData.project}
+  name="project"
   className={styles.buttons}
+  onChange={handleChange}
   style={{
     backgroundColor: selectedButton === 1 ? "#fff6ea" : "transparent",
-   
-    
     color: selectedButton === 1 ? "black" : "#fff6ea", 
   }}
 
-  onClick={() => {handleButtonClick(1);setOptions(1)}}
+  onClick={() => {handleButtonClick(1,"showcase");setOptions(1)}}
   >
     Showcase Website
   </button>
   <button
   className={styles.buttons}
+   value={formData.project}
+   name="project"
+   onChange={handleChange}
    style={{
     backgroundColor: selectedButton === 2 ? "#fff6ea" : "transparent",
     color: selectedButton === 2 ? "black" : "#fff6ea",
   }}
-  onClick={() => {handleButtonClick(2);setOptions(2)}}
+  onClick={() => {handleButtonClick(2,"commerce");setOptions(2)}}
   >
     E-commerce website
   </button>
   <button
   className={styles.buttons}
+    value={formData.project}
+    name="project"
+    onChange={handleChange}
     style={{
       backgroundColor: selectedButton === 3 ? "white" : "transparent",
       color: selectedButton === 3 ? "black" : "white",
     }}
-    onClick={() => {handleButtonClick(3);setOptions(3)}}
+    onClick={() => {handleButtonClick(3,"branding");setOptions(3)}}
   >
     Brand consultation service
   </button>
@@ -215,10 +262,7 @@ const Formm: React.FC = () => {
      className={styles.submit}
      type="submit"
      disabled={!submittable}
-     onClick={()=>{
-      if (option > 0){
-      setStep('three');setCurrent(current + 1)
-     }else {console.log('set option please',option)}}}
+     onClick={()=>{setStep('three');setCurrent(current + 1);}}
      style={{
        cursor: submittable ? 'pointer' : 'not-allowed',
      
@@ -231,7 +275,10 @@ const Formm: React.FC = () => {
  {step =='three' &&
    <div>
     <h1 className={styles.H2}>Tell us more about your project</h1>
-   <TextArea className={styles.textarea}rows={6} placeholder="Something about your great idea" maxLength={255} />
+   <TextArea className={styles.textarea}rows={6} placeholder="Something about your great idea" maxLength={255} 
+    name="msg"
+    value={formData.msg}
+    onChange={handleChange}/>
    <button
      className={styles.submit}
      type="submit"
@@ -239,28 +286,13 @@ const Formm: React.FC = () => {
           cursor: submittable ? 'pointer' : 'not-allowed',fontSize:"18px",fontWeight:"500"
         }}
    >
-     <SendOutlined style={{marginRight:'10px',fontSize:"20px",rotate:"-35deg"}}/>Send
+     <SendOutlined value={formData.project} disabled={!submittable}
+        onChange={handleChange}
+        onClick={handleSubmit} style={{marginRight:'10px',fontSize:"20px",rotate:"-35deg"}}/>Send
    </button>
    </div>
  }
       </div>
-      {/* <div  style={{ marginTop: 24 }}>
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button type="primary" onClick={() => message.success('Processing complete!')}>
-            Done
-          </Button>
-        )}
-        {current > 0 && (
-          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-            Previous
-          </Button>
-        )}
-      </div> */}
 </div>
   
    
