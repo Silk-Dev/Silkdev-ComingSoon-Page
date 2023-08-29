21ed5b76b9f7e5d15161accaf77317e0efb024f9
bedouijesser <bedouijesser@gmail.com>
2023-08-29 17:53:40 +0100

    testing client side validation

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
-    images: { unoptimized: true } 
+  output: 'export',
+  // experimental: {
+  //   serverActions: true,
+  // },
+  images: { unoptimized: true } 
 }
 
 module.exports = nextConfig
--- src/actions/SendEmail.tsx
+++ src/actions/SendEmail.tsx
@@ -1,4 +1,4 @@
-"use server";
+// "use server";
 import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/app/lib/utils";
