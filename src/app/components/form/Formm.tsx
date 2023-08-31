b358ee545e627ba382ef8ea8e533454fa19d8a4e
Amal <bentalebamal8@gmail.com>
2023-08-31 15:48:41 +0100

    fix UI bugs

--- src/app/components/form/Formm.tsx
+++ src/app/components/form/Formm.tsx
@@ -1,12 +1,9 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import { Form, ConfigProvider, Input, Space, Steps, theme } from 'antd';
-import { getAnalytics } from "firebase/analytics";
-import { FileOutlined, SendOutlined } from '@ant-design/icons'
+import { SendOutlined } from '@ant-design/icons'
 import styles from "./form.module.scss"
 import SubMsg from '../subMsg/SubMsg';
-
-import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 import { app } from '@/app/config';
 
@@ -18,6 +15,7 @@ const Formm: React.FC = () => {
   const [submittable, setSubmittable] = useState(false);
   const [viewForm, setViewForm] = useState(true);
   const [isError, setError] = useState(false);
+  const [isSubmitted, setSubmitted] = useState(false);
   const [option, setOptions] = useState(0);
   const [step, setStep] = useState('one');
   const [formData, setFormData] = useState({
@@ -67,7 +65,7 @@ const Formm: React.FC = () => {
 
   const values = Form.useWatch([], form);
 
-
+  
 
   useEffect(() => {
     form.validateFields({ validateOnly: true }).then(
@@ -78,7 +76,8 @@ const Formm: React.FC = () => {
         setSubmittable(false);
       },
     );
-
+    console.log(submittable);
+    
   }, [values, form]);
 
   const handleChange = (event:any) => {
@@ -92,9 +91,14 @@ const Formm: React.FC = () => {
 
   const handleSubmit= async () => {
     try {
+      setSubmitted(true);
+      setTimeout(() => {
+        setViewForm(false);
+      }, 2000)
       const colRef = collection(db, "formData");
       const docRef = await addDoc(colRef, formData);
       return docRef;
+      
 
     } catch (error) {
       throw error;
@@ -117,14 +121,7 @@ const Formm: React.FC = () => {
       project: selectedOption,
 
     }));
-    if (buttonId === selectedButton) {
-
-
-      setSelectedButton(null);
-    } else {
-
-      setSelectedButton(buttonId);
-    }
+    setSelectedButton(buttonId);
   };
 
   return (
@@ -150,40 +147,28 @@ const Formm: React.FC = () => {
           <Space wrap className={styles.space}>
             <button
               value={formData.project}
+              id='1'
               name="project"
-              className={styles.buttons}
+              className={selectedButton === 1 ? styles.buttons : styles.notSelected}
               onChange={handleChange}
-              style={{
-                backgroundColor: selectedButton === 1 ? "#fff6ea" : "transparent",
-                color: selectedButton === 1 ? "black" : "#fff6ea",
-              }}
-
               onClick={() => handleButtonClick(1, "showcase")}
             >
               Showcase Website
             </button>
             <button
-              className={styles.buttons}
               value="commerce"
               name="project"
               onChange={handleChange}
-              style={{
-                backgroundColor: selectedButton === 2 ? "#fff6ea" : "transparent",
-                color: selectedButton === 2 ? "black" : "#fff6ea",
-              }}
+              className={selectedButton === 2 ? styles.buttons : styles.notSelected}
               onClick={() => handleButtonClick(2, "commerce")}
             >
               E-commerce website
             </button>
             <button
-              className={styles.buttons}
+              className={selectedButton === 3 ? styles.buttons : styles.notSelected}
               value={formData.project}
               name="project"
               onChange={handleChange}
-              style={{
-                backgroundColor: selectedButton === 3 ? "white" : "transparent",
-                color: selectedButton === 3 ? "black" : "white",
-              }}
               onClick={() => handleButtonClick(3, "branding")}
             >
               Brand consultation service
@@ -199,22 +184,33 @@ const Formm: React.FC = () => {
             onChange={handleChange}
           />
 
-
+          {!isSubmitted &&
           <button
-            className={styles.submit}
-            type="submit"
-            value={formData.project}
-            onChange={handleChange}
-            onClick={handleSubmit}
-            disabled={!submittable}
-            style={{
-              cursor: submittable ? 'pointer' : 'not-allowed', fontSize: "18px", fontWeight: "500"
+          className={styles.submit}
+          type="submit"
+          value={formData.project}
+          onChange={handleChange}
+          onClick={handleSubmit}
+          disabled={!submittable}
+          style={{
+            cursor: submittable ? 'pointer' : 'not-allowed', fontSize: "18px", fontWeight: "500",marginTop:"15px"
 
-            }}
-          >
-            <SendOutlined style={{ marginRight: '10px', fontSize: "20px", rotate: "-35deg", marginBottom: "100px" }} />Send
-          </button>
-
+          }}
+        >
+          
+          <SendOutlined style={{ marginRight: '10px', fontSize: "20px", rotate: "-35deg", marginBottom: "100px",marginTop:"15px" }} />Send
+        </button>
+          }
+          {isSubmitted && 
+          <button  type="button" className={styles.submit}>
+          <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-black animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+          </svg>
+          Loading...
+      </button>
+          }
+          
         </Form>
       }
 
@@ -233,7 +229,7 @@ const Formm: React.FC = () => {
                   customIconTop: 0,
                   customIconFontSize: 32,
                   iconSize: 20,
-                  iconTop: 0, // magic for ui experience
+                  iconTop: 0, 
                   iconFontSize: 12,
                   iconSizeSM: 16,
                   dotSize: 20,
@@ -289,40 +285,27 @@ const Formm: React.FC = () => {
                 <button
                   value={formData.project}
                   name="project"
-                  className={styles.buttons}
                   onChange={handleChange}
-                  style={{
-                    width: ' 100%',
-                    backgroundColor: selectedButton === 1 ? "#fff6ea" : "transparent",
-                    color: selectedButton === 1 ? "black" : "#fff6ea",
-                  }}
+                  className={selectedButton === 1 ? styles.buttons : styles.notSelected}
 
                   onClick={() => { handleButtonClick(1, "showcase"); setOptions(1) }}
                 >
                   Showcase Website
                 </button>
                 <button
-                  className={styles.buttons}
                   value={formData.project}
                   name="project"
                   onChange={handleChange}
-                  style={{
-                    backgroundColor: selectedButton === 2 ? "#fff6ea" : "transparent",
-                    color: selectedButton === 2 ? "black" : "#fff6ea",
-                  }}
+                  className={selectedButton === 2 ? styles.buttons : styles.notSelected}
                   onClick={() => { handleButtonClick(2, "commerce"); setOptions(2) }}
                 >
                   E-commerce website
                 </button>
                 <button
-                  className={styles.buttons}
                   value={formData.project}
                   name="project"
                   onChange={handleChange}
-                  style={{
-                    backgroundColor: selectedButton === 3 ? "white" : "transparent",
-                    color: selectedButton === 3 ? "black" : "white",
-                  }}
+                  className={selectedButton === 3 ? styles.buttons : styles.notSelected}
                   onClick={() => { handleButtonClick(3, "branding"); setOptions(3) }}
                 >
                   Brand consultation service
@@ -349,16 +332,32 @@ const Formm: React.FC = () => {
                 name="msg"
                 value={formData.msg}
                 onChange={handleChange} />
-              <button
-                className={styles.submit}
-                type="submit"
-                style={{
-                  cursor: submittable ? 'pointer' : 'not-allowed', fontSize: "18px", fontWeight: "500"
-                }}
-              >
-                <SendOutlined onChange={handleChange}
-                  onClick={handleSubmit} style={{ marginRight: '10px', fontSize: "20px", rotate: "-35deg", marginBottom: "100px" }} />Send
-              </button>
+              {!isSubmitted &&
+          <button
+          className={styles.submit}
+          type="submit"
+          value={formData.project}
+          onChange={handleChange}
+          onClick={handleSubmit}
+          disabled={!submittable}
+          style={{
+            cursor: submittable ? 'pointer' : 'not-allowed', fontSize: "18px", fontWeight: "500",marginTop:"10px"
+
+          }}
+        >
+          
+          <SendOutlined style={{ marginRight: '10px', fontSize: "20px", rotate: "-35deg", marginBottom: "100px",marginTop:"10px" }} />Send
+        </button>
+          }
+          {isSubmitted && 
+          <button  type="button" className={styles.submit}>
+          <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-black animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+          </svg>
+          Loading...
+      </button>
+          }
             </Form>
 
           }
@@ -367,8 +366,6 @@ const Formm: React.FC = () => {
       {!viewForm &&
         <SubMsg result={isError} />
       }
-
-
     </>
   );
 };
--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '/public/img/logo.png'
+import styles from './header.module.scss'
 const Header = () => {
   return (
     <div>
-      <Image  alt='' width={190} height={80} src={logo}/>
+      <Image  alt='' width={160} height={80} src={logo} className={styles.logo}/>
     </div>
   )
 }
