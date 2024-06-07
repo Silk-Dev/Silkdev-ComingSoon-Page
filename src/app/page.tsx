61372f0050102770f30ea3503f2e8259eb9f724a
bedouijesser <bedouijesser@gmail.com>
2024-06-07 07:26:17 +0100

    some commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,25 @@ import HeroSection from './components/heroSection/HeroSection'
 import Header from './components/header/Header'
 import Slider from './components/marquee/Marqueee'
 import Form from './components/form/Formm'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJEOKqWq6dZfAZYpexMPBf6OVUGHlaHA0",
+  authDomain: "silkdev-coming-soon.firebaseapp.com",
+  projectId: "silkdev-coming-soon",
+  storageBucket: "silkdev-coming-soon.appspot.com",
+  messagingSenderId: "889994104678",
+  appId: "1:889994104678:web:c18ad4e35a83bde6b742a7",
+  measurementId: "G-EVY97VQ3P2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function Home() {
   return (
@@ -12,7 +30,6 @@ export default function Home() {
       <Header />
       <HeroSection />
       <Form/>
-      
     </main>
   )
 }
